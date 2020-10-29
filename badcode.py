2aeff1e048f9445f2aebc4025d5c84088e59f013
Jatinder Singh <jatinder.singh@netsolutions.com>
2020-10-29 04:20:06 +0000

    bad code

---
+++ badcode.py
@@ -0,0 +1,10 @@
+num1 = 10
+num2 = 20
+print('Sum:', (num1 + num2))
+big_line = 'This is a big line to test the code linting using github action workflows. Pylint workflow is added in .github folder and pushed in the main branch. This is just a commit test with long line.'
+
+list = ['john', 'doe']
+if len(list):
+	print("list is filled with names")
+else:
+	print("List is empty")
